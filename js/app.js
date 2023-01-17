9b12b927c11e066a08b9e620ffcf48d9be9c3085
Luis Rangel <lr2315490@gmail.com>
2023-01-17 14:53:37 -0600

    Terminado

--- js/app.js
+++ js/app.js
@@ -3,21 +3,21 @@ console.log('conetado...')
 const productData = [
 {
 index: '01',
-name: 'Yellow One',
+name: 'Oufit One',
 Image: 'img1.jpg',
 des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, aperiam, iusto nobis quidem quisquam vitae nemo doloribus libero aliquam fugit culpa molestias porro ad quis soluta ipsam sequi totam. Est.'
 }
 ,
 {
 index: '02',
-name: 'Yellow Two',
+name: 'Oufit Two',
 Image: 'img2.jpg',
 des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, aperiam, iusto nobis quidem quisquam vitae nemo doloribus libero aliquam fugit culpa molestias porro ad quis soluta ipsam sequi totam. Est.'
 }
 ,
 {
 index: '03',
-name: 'Yellow Three',
+name: 'Oufit Three',
 Image: 'img3.jpg',
 des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, aperiam, iusto nobis quidem quisquam vitae nemo doloribus libero aliquam fugit culpa molestias porro ad quis soluta ipsam sequi totam. Est.'
 }
@@ -33,7 +33,7 @@ let smProductDes = document.querySelector('.sm-product-des')
 let productImgContainer = document.querySelector('.product-img-container')
 let productImg = document.querySelector('.product-img')
 let backdropImg = document.querySelector('.backdrop-img')
-let productDetail = document.querySelector('.product-detail')
+let productDetail = document.querySelector('.detail-product')
 let productName = document.querySelector('.product-name')
 let productDes = document.querySelector('.product-des')
 
@@ -51,6 +51,19 @@ nextBtn.addEventListener('click', () => {
     smProductDes.innerHTML = productData[currentProduct].des.slice(0, 80)
     smImgContainer.classList.add('slide')
     productImgContainer.classList.add('slide')
-    backdropImg.classList.add('slide')
-    productDetail.classList.add('slide')
+    backdropImg.classList.add('fede')
+    productDetail.classList.add('fede')
+
+    setTimeout(() =>{
+        productName.innerHTML = productData[currentProduct].name
+        productDes.innerHTML = productData[currentProduct].des
+        smImg.src = productImg.src = backdropImg.src = `img/${productData[currentProduct].Image}`
+    }, 500)
+
+    setTimeout(() =>{
+    smImgContainer.classList.remove('slide')
+    productImgContainer.classList.remove('slide')
+    backdropImg.classList.remove('fede')
+    productDetail.classList.remove('fede')
+    }, 1000)
 })
